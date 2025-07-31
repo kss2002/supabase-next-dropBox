f3e834cf265a41dc4a3e45db13d268996c2efb07
seong su kim <know.warehouse02@gmail.com>
2025-07-31 23:58:23 +0900

    re:

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,7 @@ export default function Home() {
   return (
     <main>
       <h1 className="text-3xl font-bold underline">Welcome to Supa Drop!</h1>
-      <p className="mt-4">
-        This is a simple example of a Next.js application using Supabase for
-        database operations.
-      </p>
-      <p className="mt-2">
-        You can start building your application by modifying the code in the{' '}
-        <code>app/page.tsx</code> file.
-      </p>
+      
     </main>
   );
 }
