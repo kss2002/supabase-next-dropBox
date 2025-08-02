84dbef9100b9635a8097b6a57c16a3f32ab93de4
seong su kim <know.warehouse02@gmail.com>
2025-08-02 22:31:16 +0900

    ui tsx add

---
+++ app/ui.tsx
@@ -0,0 +1,13 @@
+export default function UI() {
+  return (
+    <main className="w-full p-2 flex flex-col gap-4">
+      {/* Logo */}
+
+      {/* Search Component */}
+
+      {/* File Drag&Drop Zone */}
+
+      {/* Dropbox Image List */}
+    </main>
+  );
+}
