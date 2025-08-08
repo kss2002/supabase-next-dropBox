aa7d6c8c3fbf4612973033f5659f7a5586802d4c
seong su kim <know.warehouse02@gmail.com>
2025-08-08 20:58:28 +0900

    error styling

--- app/error.tsx
+++ app/error.tsx
@@ -1,13 +1,16 @@
 'use client';
 
 import Link from 'next/link';
+import style from './404.module.css';
 
 export default function Errorpage() {
   return (
     <>
-      <div>
-        <h1>오류가 발생되었습니다</h1>
-        <Link href="/">홈으로 돌아가기</Link>
+      <div className={style.not__main}>
+        <h1 className={style.not__h1}>오류가 발생되었습니다</h1>
+        <button className={style.not__button}>
+          <Link href="/">홈으로 돌아가기</Link>
+        </button>
       </div>
     </>
   );
