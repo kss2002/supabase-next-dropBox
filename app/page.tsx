f3c70d13e7d6d902104ce8888c05e9f599b94930
seong su kim <know.warehouse02@gmail.com>
2025-08-13 23:09:18 +0900

    tailwind modify:

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="text-3xl font-bold underline">Welcome to Supa Drop!</h1>
-      
+      <h1 className="text-3xl font-bold">Welcome to Supa Drop!</h1>
     </main>
   );
 }
