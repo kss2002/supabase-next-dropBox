1c55d6c163d9dbfb53a3bba7c93d0c6be0e87c3a
seong su kim <know.warehouse02@gmail.com>
2025-08-22 20:05:48 +0900

    메인 페이지 빈 태그 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-3xl font-bold">Welcome to Supa Drop!</h1>
-    </main>
+    <>
+      <main>
+        <h1 className="text-3xl font-bold">Welcome to Supa Drop!</h1>
+      </main>
+    </>
   );
 }
